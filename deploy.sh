591ede450bf4648c92a6209d5a9d18d0f91d3a6d
yakumioto <yaku.mioto@gmail.com>
2017-11-20 14:09:32 +0800

    UpdateAt: 2017-11-20 14:09:32

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,4 @@
 set -ev
 export TZ='Asia/Shanghai'
 
-npm install hexo-cli -g
-
-npm install
-
-git config --global user.email "yaku.mioto@gmail.com"
-git config --global user.name "yakumioto"
-
 hexo g -d
