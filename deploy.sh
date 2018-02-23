825ab2925a9bcd57f6789291690cddf955a56ff7
yakumioto <yaku.mioto@gmail.com>
2018-02-23 15:03:38 +0800

    UpdateAt: 2018-02-23 15:03:38

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,7 @@ export TZ='Asia/Shanghai'
 git config --global user.email "yaku.mioto@gmail.com"
 git config --global user.name "yakumioto"
 
+npm install --save hexo-filter-flowchart
+npm install --save hexo-filter-sequence
+
 hexo clean && hexo d -g
