4bb64c08e6a72cf9d1a9d2f0f7f9b35f17c6424b
yakumioto <yaku.mioto@gmail.com>
2019-04-28 11:00:19 +0800

    UpdateAt: 2019-04-28 11:00:19

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ git add --all
 
 git commit -m "UpdateAt: $(date "+%Y-%m-%d %H:%M:%S")"
 
-git push origin master
+git push origin --all
--- source/_drafts/GoNegroni详解.md
+++
@@ -1,6 +0,0 @@
----
-title: Go Negroni 原理解析
-date: 2018-03-09 16:41:45
-tags:
-  - Go
----
